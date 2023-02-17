46639c9e3c389545053ffab212718e8f270bc69a
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2023-02-17 00:44:50 -0500

    after filtering type array is returned

--- A_Walia_RealityShows/src/app/content-type-filter.pipe.ts
+++ A_Walia_RealityShows/src/app/content-type-filter.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './helper-files/content-interface';
 
 @Pipe({
   name: 'contentTypeFilter'
 })
 export class ContentTypeFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(cards: Content[], typeFilter?: string): Content[] {
+   if(!typeFilter){
+    return cards.filter(card => !card.type);
+   }
+   return cards.filter(card => card.type === typeFilter);
   }
 
 }
