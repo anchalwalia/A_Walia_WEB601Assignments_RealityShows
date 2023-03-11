46dee337d601f29b31b247f22fe2df736ee7e777
anchalwalia <105453237+anchalwalia@users.noreply.github.com>
2023-03-10 19:47:02 -0500

    added html,css with function to show content card

--- A_Walia_RealityShows/src/app/app.component.ts
+++ A_Walia_RealityShows/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShowsService } from './services/shows.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'A_Walia_RealityShows';
+  contentId: number = 4;
+  topContent: any = {};
+  imgurl: string ='https://3.bp.blogspot.com/-s22fJGFLi3U/W2W4VEGnM0I/AAAAAAAADSo/0jYvQPESyQsXnRTx0gdN8B3AaSHHCLfuACLcBGAs/s1600/Screen%2BShot%2B2018-08-04%2Bat%2B3.29.01%2BPM.png';
+
+  
+  constructor(private ShowsService: ShowsService){}
+
+  clickLog(content: any){
+    console.log(`ID: ${content.id}`);
+    console.log(`Title: ${content.title}`);
+  }
+
+  ngOnInit(){
+    this.ShowsService.getContentById(this.contentId).subscribe(content => this.topContent = content);
+  }
 }
